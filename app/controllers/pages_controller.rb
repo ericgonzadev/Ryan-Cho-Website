c7472c15f1c96a8b986985bee224e27f493516ce
Eric <twinpaireric@gmail.com>
2018-01-03 15:48:08 -0800

    Add edit page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
+
   def index
     @information = Information.first
     @freelance_work = Project.retrieve_projects("Freelance Work")
     @portfolio = Project.retrieve_projects("Portfolio")
     @message = Message.new
   end
+
+  def edit
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'pages#index'
 
+  get '/edit', to: 'pages#edit'
   get 'information/edit', to: 'information#edit'
   resources :information, only: [:index, :update]
   resources :projects
