6e46f789e6fb52446fd4b52b8f648290f2799f23
Eric <twinpaireric@gmail.com>
2018-01-03 12:42:54 -0800

    Generate messages model and db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103015355) do
+ActiveRecord::Schema.define(version: 20180103204028) do
 
   create_table "information", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 20180103015355) do
     t.datetime "updated_at",           null: false
     t.string   "email"
     t.string   "phone"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "subject"
+    t.text     "message"
+    t.string   "captcha"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "projects", force: :cascade do |t|
