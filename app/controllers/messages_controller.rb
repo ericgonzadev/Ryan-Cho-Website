2dd4f122b14b7854a4cbee59f491e0ce885bb1ae
Eric Gonzalez <ericextendview@gmail.com>
2020-01-18 11:45:10 -0800

    Add new captcha image

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,9 +13,9 @@ class MessagesController < ApplicationController
       flash[:success] = "Message Received! I will be in touch with you soon."
       redirect_to "/#contact"
     else
-      if (message_params["name"].blank? || message_params["email"].blank? || message_params["message"].blank? || message_params["captcha"].blank?) && (message_params["captcha"] =~ /\Aw68hp\z/i).nil?
+      if (message_params["name"].blank? || message_params["email"].blank? || message_params["message"].blank? || message_params["captcha"].blank?) && (message_params["captcha"] =~ /\Aczarface\z/i).nil?
         flash[:danger] = "ERRORS=- Required field was left blank=- Captcha text did not match image text"
-      elsif (message_params["captcha"] =~ /\Aw68hp\z/i).nil?
+      elsif (message_params["captcha"] =~ /\Aczarface\z/i).nil?
         flash[:danger] = "ERROR=- Captcha text did not match image text"
       elsif message_params["name"].blank? || message_params["email"].blank? || message_params["message"].blank? || message_params["captcha"].blank?
         flash[:danger] = "ERROR=- Required field was left blank"
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
 
-  VALID_CAPTCHA_REGEX = /\Aw68hp\z/i
+  VALID_CAPTCHA_REGEX = /\Aczarface\z/i
   validates_presence_of :name, :email, :message, :captcha
   validates :captcha, format: {with: VALID_CAPTCHA_REGEX}
 end
